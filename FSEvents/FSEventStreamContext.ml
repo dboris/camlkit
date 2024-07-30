a5d9e1da2262398d316afc5bc9cf4544c285faef
Thomas Gazagnaire <thomas@gazagnaire.org>
2024-07-30 16:29:11 +0200

    Update FSEVents bindings
    This uses:
    - https://github.com/dboris/camlkit-bindings-generator/pull/3
    - https://github.com/dboris/camlkit-bindings-generator/pull/4
    - https://github.com/dboris/camlkit-bindings-generator/pull/5

--- FSEvents/FSEventStreamContext.ml
+++ FSEvents/FSEventStreamContext.ml
@@ -14,6 +14,20 @@ let copyDescription = field t "copyDescription" (ptr (ptr void))
 
 let () = seal t
 
+let init
+    ~version:version_v
+    ?info:(info_v = from_voidp void null)
+    ?retain:(retain_v = from_voidp (ptr void) null)
+    ?release:(release_v = from_voidp (ptr void) null)
+    ?copyDescription:(copyDescription_v = from_voidp (ptr void) null)
+    () =
+  let t = make t in
+  setf t version version_v;
+  setf t info info_v;
+  setf t retain retain_v;
+  setf t release release_v;
+  setf t copyDescription copyDescription_v;
+t
 let version t = getf t version
 let info t = getf t info
 let retain t = getf t retain
