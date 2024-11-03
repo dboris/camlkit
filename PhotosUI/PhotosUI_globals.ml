7ffd45a37eac5124a827ee5d1b8903182672d992
Boris D <boris.dob@gmail.com>
2024-11-03 09:11:20 -0800

    Update PhotosUI_globals.

--- PhotosUI/PhotosUI_globals.ml
+++ PhotosUI/PhotosUI_globals.ml
@@ -1,10 +1,54 @@
 (* auto-generated, do not modify *)
 
+[@@@ocaml.warning "-33"]
+open Runtime
+open Objc
+
+let _PHProjectCategoryBook = new_string "PHProjectCategoryBook"
+let _PHProjectCategoryCalendar = new_string "PHProjectCategoryCalendar"
+let _PHProjectCategoryCard = new_string "PHProjectCategoryCard"
+let _PHProjectCategoryOther = new_string "PHProjectCategoryOther"
+let _PHProjectCategoryPrints = new_string "PHProjectCategoryPrints"
+let _PHProjectCategorySlideshow = new_string "PHProjectCategorySlideshow"
+let _PHProjectCategoryUndefined = new_string "PHProjectCategoryUndefined"
+let _PHProjectCategoryWallDecor = new_string "PHProjectCategoryWallDecor"
+let _PHProjectTypeUndefined = new_string "PHProjectTypeUndefined"
+let _PHLivePhotoViewContentModeAspectFill = 1
+let _PHLivePhotoViewContentModeAspectFit = 0
 let _PHLivePhotoViewPlaybackStyleFull = 1
 let _PHLivePhotoViewPlaybackStyleHint = 2
 let _PHLivePhotoViewPlaybackStyleUndefined = 0
+let _PHPickerCapabilitiesCollectionNavigation = 4
+let _PHPickerCapabilitiesNone = 0
+let _PHPickerCapabilitiesSearch = 1
+let _PHPickerCapabilitiesSelectionActions = 8
+let _PHPickerCapabilitiesSensitivityAnalysisIntervention = 16
+let _PHPickerCapabilitiesStagingArea = 2
 let _PHPickerConfigurationAssetRepresentationModeAutomatic = 0
 let _PHPickerConfigurationAssetRepresentationModeCompatible = 2
 let _PHPickerConfigurationAssetRepresentationModeCurrent = 1
+let _PHPickerConfigurationSelectionContinuous = 2
+let _PHPickerConfigurationSelectionContinuousAndOrdered = 3
 let _PHPickerConfigurationSelectionDefault = 0
 let _PHPickerConfigurationSelectionOrdered = 1
+let _PHPickerModeCompact = 1
+let _PHPickerModeDefault = 0
+let _PHProjectCreationSourceAlbum = 2
+let _PHProjectCreationSourceMemory = 3
+let _PHProjectCreationSourceMoment = 4
+let _PHProjectCreationSourceProject = 20
+let _PHProjectCreationSourceProjectBook = 21
+let _PHProjectCreationSourceProjectCalendar = 22
+let _PHProjectCreationSourceProjectCard = 23
+let _PHProjectCreationSourceProjectExtension = 26
+let _PHProjectCreationSourceProjectPrintOrder = 24
+let _PHProjectCreationSourceProjectSlideshow = 25
+let _PHProjectCreationSourceUndefined = 0
+let _PHProjectCreationSourceUserSelection = 1
+let _PHProjectSectionTypeAuxiliary = 3
+let _PHProjectSectionTypeContent = 2
+let _PHProjectSectionTypeCover = 1
+let _PHProjectSectionTypeUndefined = 0
+let _PHProjectTextElementTypeBody = 0
+let _PHProjectTextElementTypeSubtitle = 2
+let _PHProjectTextElementTypeTitle = 1
