cb85f47debe431024281f57f07b3a01d5cdb2991
Boris D <boris.dob@gmail.com>
2025-03-23 18:04:07 -0700

    Rename effect method bindings.

--- UIKit/UIFocusEffectClass.ml
+++ UIKit/UIFocusEffectClass.ml
@@ -10,4 +10,4 @@ open CoreText
 
 (** Apple docs: {{:https://developer.apple.com/documentation/uikit/uifocuseffect?language=objc}UIFocusEffect} *)
 
-let effect self = msg_send ~self ~cmd:(selector "effect") ~typ:(returning id)
+let effect' self = msg_send ~self ~cmd:(selector "effect") ~typ:(returning id)
